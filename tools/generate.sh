4e8ecdf205dc87b8ed783ec14c55d889f38a421b
David Koski <dkoski@apple.com>
2020-12-26 21:13:34 -0800

    remove ay

--- tools/generate.sh
+++ tools/generate.sh
@@ -22,7 +22,6 @@ guilds=(
 	'ebonhand 783828985'
 	'ewoking 714847433'
 	'rambos 739491776'
-	'younglings 951659657'
 	'brother 494962926'
 	'lazerfaces 469344695'
 	'kotor 815925164'
@@ -30,21 +29,46 @@ guilds=(
 	'esb 496642589'
 )
 
-for g in $guilds ; do 
-	args=("${(@s/ /)g}")
-	echo "==============="
-	echo "$args[1]"
+# prime the pipe -- fetch the first guild
+echo "==============="
+echo "FETCH: $guilds[1]"
+args=("${(@s/ /)guilds[1]}")
+
+echo "~/bin/swgoh-tool --fetch $args[2] $args[1]"
+time ~/bin/swgoh-tool --fetch $args[2] $args[1] || exit 1
+
+
+for ((i = 1; i < $#guilds; i++)) ; do 
+	args=("${(@s/ /)guilds[i]}")
 	
-	echo "FETCH: $guild"
+	# now build the guild we just fetched
+	echo "~/bin/swgoh-tool --guild $args[1].json --site docs"
+	time ~/bin/swgoh-tool --guild $args[1].json --site docs &
+	
+	# and fetch the next one
+	
+	echo "==============="
+	args=("${(@s/ /)guilds[i + 1]}")
+	echo "FETCH: $args"
 
 	echo "~/bin/swgoh-tool --fetch $args[2] $args[1]"
 	time ~/bin/swgoh-tool --fetch $args[2] $args[1] || exit 1
 	
-	echo "~/bin/swgoh-tool --guild $args[1].json --site docs"
-	time ~/bin/swgoh-tool --guild $args[1].json --site docs
+	# wait for both to complete
+	wait
+	
 done
+
+# finish the pipeline
+args=("${(@s/ /)guilds[$#guilds]}")
+echo "~/bin/swgoh-tool --guild $args[1].json --site docs"
+time ~/bin/swgoh-tool --guild $args[1].json --site docs &
 
 echo "==============="
 echo "ALLIANCE"
 
 time ~/bin/swgoh-tool --alliance docs *.json
+
+wait
+
+ret=$?; times; exit "$ret"
