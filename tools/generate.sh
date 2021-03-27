bb014f95e5c5dc8f31a5246f305e927d5ebecb8b
Carovnicek <Jakub.Vincenec@gmail.com>
2021-03-27 14:01:55 +0100

    Updated generate.sh to contain correct guilds and swgoh-tool path.

--- tools/generate.sh
+++ tools/generate.sh
@@ -1,32 +1,11 @@
 #!/bin/zsh
 
+alias swgoh-tool=/Applications/swgoh-tool.app/Contents/MacOS/swgoh-tool
 
 guilds=(
-	'baltic 228765519'
-	'exile 656997348'
-	'hope 369351434'
-	'hoth 329251861'
-	'jnj 932325566'
-	'llvy 454754726'
-	'lop 388924554'
-	'mandalorians 591287488'
-	'porgs 986843228'
-	'sbr 141812836'
-	'twd 414232583'
-	'wk 221913824'
-	'tumbleweed 115961515'
-	'templar 135722653'
-	'kotd 126515217'
-	'honor 558111281'
-	'phoenixhawk 767641473'
-	'ebonhand 783828985'
-	'ewoking 714847433'
-	'rambos 739491776'
-	'brother 494962926'
-	'lazerfaces 469344695'
-	'kotor 815925164'
-	'thrawn 464574793'
-	'han 445419677'
+	'oae 979789966'
+	'bad 619182389'
+	'end 845439946'
 )
 
 # prime the pipe -- fetch the first guild
@@ -34,16 +13,16 @@ echo "==============="
 echo "FETCH: $guilds[1]"
 args=("${(@s/ /)guilds[1]}")
 
-echo "~/bin/swgoh-tool --fetch $args[2] $args[1]"
-time ~/bin/swgoh-tool --fetch $args[2] $args[1] || exit 1
+echo "swgoh-tool --fetch $args[2] $args[1]"
+time swgoh-tool --fetch $args[2] $args[1] || exit 1
 
 
 for ((i = 1; i < $#guilds; i++)) ; do 
 	args=("${(@s/ /)guilds[i]}")
 	
 	# now build the guild we just fetched
-	echo "~/bin/swgoh-tool --guild $args[1].json --site docs"
-	time ~/bin/swgoh-tool --guild $args[1].json --site docs &
+	echo "swgoh-tool --guild $args[1].json --site docs"
+	time swgoh-tool --guild $args[1].json --site docs &
 	
 	# and fetch the next one
 	
@@ -51,8 +30,8 @@ for ((i = 1; i < $#guilds; i++)) ; do
 	args=("${(@s/ /)guilds[i + 1]}")
 	echo "FETCH: $args"
 
-	echo "~/bin/swgoh-tool --fetch $args[2] $args[1]"
-	time ~/bin/swgoh-tool --fetch $args[2] $args[1] || exit 1
+	echo "swgoh-tool --fetch $args[2] $args[1]"
+	time swgoh-tool --fetch $args[2] $args[1] || exit 1
 	
 	# wait for both to complete
 	wait
@@ -61,13 +40,13 @@ done
 
 # finish the pipeline
 args=("${(@s/ /)guilds[$#guilds]}")
-echo "~/bin/swgoh-tool --guild $args[1].json --site docs"
-time ~/bin/swgoh-tool --guild $args[1].json --site docs &
+echo "swgoh-tool --guild $args[1].json --site docs"
+time swgoh-tool --guild $args[1].json --site docs &
 
 echo "==============="
 echo "ALLIANCE"
 
-time ~/bin/swgoh-tool --alliance docs *.json
+time swgoh-tool --alliance docs *.json
 
 wait
 
